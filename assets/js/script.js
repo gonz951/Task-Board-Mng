b7f044081091c758377ffe419051de7d126162a0
Fel <felgonzalez951@gmail.com>
2024-04-01 15:52:46 -0600

    changed some variables for email to date, need to tweak the others later

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,13 +42,13 @@ $(document).ready(function () {
 // ! FIX NAMING 
 $( function() {
     var dialog, form,
- 
-      // From http://www.whatwg.org/specs/web-apps/current-work/multipage/states-of-the-type-attribute.html#e-mail-state-%28type=email%29
-      emailRegex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
-      name = $( "#name" ),
-      email = $( "#email" ),
+      name = $( "#task-name" ),
+    //   ! Still need to change these
+      date = $( "#duedate" ),
+    //   * Fixed any traces of 'email', but should check later
       password = $( "#password" ),
-      allFields = $( [] ).add( name ).add( email ).add( password ),
+    //   ! Still need to change these
+      allFields = $( [] ).add( name ).add( date ).add( password ),
       tips = $( ".validateTips" );
  
     function updateTips( t ) {
@@ -85,18 +85,18 @@ $( function() {
       var valid = true;
       allFields.removeClass( "ui-state-error" );
  
-      valid = valid && checkLength( name, "username", 3, 16 );
-      valid = valid && checkLength( email, "email", 6, 80 );
+      valid = valid && checkLength( name, "task-name", 3, 16 );
+      valid = valid && checkLength( date, "duedate", 6, 80 );
       valid = valid && checkLength( password, "password", 5, 16 );
  
       valid = valid && checkRegexp( name, /^[a-z]([0-9a-z_\s])+$/i, "Username may consist of a-z, 0-9, underscores, spaces and must begin with a letter." );
-      valid = valid && checkRegexp( email, emailRegex, "eg. ui@jquery.com" );
+      valid = valid && checkRegexp( date, emailRegex, "eg. ui@jquery.com" );
       valid = valid && checkRegexp( password, /^([0-9a-zA-Z])+$/, "Password field only allow : a-z 0-9" );
  
       if ( valid ) {
         $( "#users tbody" ).append( "<tr>" +
           "<td>" + name.val() + "</td>" +
-          "<td>" + email.val() + "</td>" +
+          "<td>" + date.val() + "</td>" +
           "<td>" + password.val() + "</td>" +
         "</tr>" );
         dialog.dialog( "close" );
@@ -110,7 +110,7 @@ $( function() {
       width: 350,
       modal: true,
       buttons: {
-        "Create an account": addUser,
+        "Create a Task": addUser,
         Cancel: function() {
           dialog.dialog( "close" );
         }
